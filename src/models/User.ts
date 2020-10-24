48f71d61f59e917c1d0401af94bc7c81fedceacf
Diovane1103 <diovane.rossato@ilegra.com>
2020-10-24 18:40:20 -0300

    add controllers, models and views to User table, only with create, index and show capability, adding argon to hash the password

---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  age: number;
+}
